a8341842553993c5294f57fbbdf3e0b5ddc10a71
viyas52 <vyas.illusionist@gmail.com>
2024-06-20 20:20:21 +0530

    common.py updated

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,6 +1,8 @@
 import os
 from src.exception import CustomException
+from box.exceptions import BoxValueError
 import yaml
+import sys
 from src import logging
 import json
 import joblib
@@ -28,12 +30,12 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            logging.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
-        raise e
+        raise CustomException(e,sys)
     
 
 
@@ -48,7 +50,7 @@ def create_directories(path_to_directories: list, verbose=True):
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
-            logger.info(f"created directory at: {path}")
+            logging.info(f"created directory at: {path}")
 
 
 @ensure_annotations
@@ -62,7 +64,7 @@ def save_json(path: Path, data: dict):
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
 
-    logger.info(f"json file saved at: {path}")
+    logging.info(f"json file saved at: {path}")
 
 
 
@@ -80,7 +82,7 @@ def load_json(path: Path) -> ConfigBox:
     with open(path) as f:
         content = json.load(f)
 
-    logger.info(f"json file loaded succesfully from: {path}")
+    logging.info(f"json file loaded succesfully from: {path}")
     return ConfigBox(content)
 
 
@@ -93,7 +95,7 @@ def save_bin(data: Any, path: Path):
         path (Path): path to binary file
     """
     joblib.dump(value=data, filename=path)
-    logger.info(f"binary file saved at: {path}")
+    logging.info(f"binary file saved at: {path}")
 
 
 @ensure_annotations
@@ -107,7 +109,7 @@ def load_bin(path: Path) -> Any:
         Any: object stored in the file
     """
     data = joblib.load(path)
-    logger.info(f"binary file loaded from: {path}")
+    logging.info(f"binary file loaded from: {path}")
     return data
 
 
