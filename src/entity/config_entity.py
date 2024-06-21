f15f396ce7fc36e1877e6417b962c09aa968d5bb
viyas52 <vyas.illusionist@gmail.com>
2024-06-21 23:40:00 +0530

    data validation started

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -9,4 +9,9 @@ class DataIngestionConfig:
     unzip_dir: Path
     
     
-    
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
